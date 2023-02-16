ec3c5716fcb632852a1099e82802b0ef0712bba3
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2023-02-16 16:27:27 +0100

    Commit du 16.02.23 après-midi
    Changement de drapeau aléatoire

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const image = document.querySelector("#container img");
+const answer = document.getElementById("answer");
+const validation = document.getElementById("validation");
 const true_response = document.getElementById("true_response");
 const false_response = document.getElementById("false_response");
 
-fetch("https://flagcdn.com/fr/codes.json")
+fetch("https://flagcdn.com/en/codes.json")
     .then(response => response.json())
     .then((data) => {
-        console.log(data);
-        console.log(data.length);
+        
+        function imageUrlBuilder(){
+            let chosen_country_random = Math.floor(Math.random() * Object.keys(data).length);
+            let country_domain = Object.keys(data)[chosen_country_random];
+            let imageurl = "https://flagcdn.com/w320/" + country_domain + ".png"
+            console.log(imageurl);
+            return imageurl;
+        }
+
+        validation.addEventListener("click", function(){
+            image.src = imageUrlBuilder();
+            console.log(answer.value);
+        })
+
+        function isValid(verifier){
+            if(verifier){
+
+            }
+            else{
+                
+            }
+        }
+
+
+
 
         
     })
     
 
-function ImageUrlBuilder(){
-
-}
